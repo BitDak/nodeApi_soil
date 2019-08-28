5572399d76ebfc3b7c6f789f3144063dfe97a7a6
Bitdak <bitdak@126.com>
2019-08-28 14:54:14 +0800

    Update mysqlConf.js

--- conf/mysqlConf.js
+++ conf/mysqlConf.js
@@ -1,10 +1,10 @@
 module.exports = {
     mysql: {
-        host: 'cdb-hf5nuibm.cd.tencentcdb.com',
-        port:'10002',
+        host: 'mysqlDatabaseIP',
+        port:'3306',
         user: 'root',
-        password: 'lyyc1413',
-        database:'express-mysql-demo',
+        password: 'password',
+        database:'mysqlDatabase',
         // 最大连接数，默认为10
         connectionLimit: 10
     }
